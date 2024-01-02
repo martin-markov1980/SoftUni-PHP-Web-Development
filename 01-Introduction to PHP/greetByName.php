6924b5ef880c7bca149cac9fcda6aa798a1f694b
Martin Markov <martin.markov1980@gmail.com>
2024-01-02 21:13:35 +0000

    Git init, project structure and completed task "Greet by name".

---
+++ 01-Introduction to PHP/greetByName.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    1. Greet by name
+    Write a program that reads a name from the console and prints "Hello, {name}!"
+*/
+
+echo "Enter a name below please:" . PHP_EOL;
+$name = readline();
+
+echo "Hello, {$name}!";
