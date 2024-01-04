63f2f027441016fbca9c970e50ca9e143a17e9e9
Martin Markov <martin.markov1980@gmail.com>
2024-01-04 22:31:46 +0000

    Completed "Student Information" task.

---
+++ 02-Introduction to PHP Exercises/StudentInformation.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+  Problem 1. Student Information
+  You will be given 3 lines of input – student name, age and average grade. 
+  Your task is to print all the info about the student in the following format: "Name: {student name}, Age: {student age}, Grade: {student grade}".
+*/
+
+$name = readline("Student name:");
+$age = readline("Age:");
+$grade = readline("Grade:");
+
+echo "Name: {$name}, Age: {$age}, Grade: {$grade}";
