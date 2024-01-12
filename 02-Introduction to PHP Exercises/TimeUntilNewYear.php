151e2ba1875d658e8d8cf058a0d703b85c4d4f74
Martin Markov <martin.markov1980@gmail.com>
2024-01-12 10:14:44 +0000

    Completed "Problem 14. * *Time Until New Year" task.

--- 02-Introduction to PHP Exercises/TimeUntilNewYear.php
+++ 02-Introduction to PHP Exercises/TimeUntilNewYear.php
@@ -1,6 +1,7 @@
 <?php
 
 /*
+  Problem 14. * *Time Until New Year
   Write a PHP script TimeUntilNewYear.php. Use the built-in function getdate() to get the current date and time. 
   Print how many hours, minutes and seconds are left until New Year and how many days, hours, minutes and seconds are left in a counter format . 
   Look at examples below to get a better idea. The examples show the current date and time in "d-m-Y G:i:s" format. 
@@ -18,7 +19,6 @@ $minutesUntilNewYear = intval($secondsUntilNewYear / 60);
 $hoursUntilNewYear = intval($minutesUntilNewYear / 60);
 $daysUntilNewYear = intval($hoursUntilNewYear / 24);
 
-$formattedDaysUntilNewYear = intval($daysUntilNewYear);
 $formattedHoursUntilNewYear = $hoursUntilNewYear - $daysUntilNewYear * 24;
 $formattedMinutesUntilNewYear = $minutesUntilNewYear - $hoursUntilNewYear * 60;
 $formattedSecondsUntilNewYear = $secondsUntilNewYear - $minutesUntilNewYear * 60;
@@ -26,4 +26,4 @@ $formattedSecondsUntilNewYear = $secondsUntilNewYear - $minutesUntilNewYear * 60
 echo "Hours until new year : {$hoursUntilNewYear}" . PHP_EOL;
 echo "Minutes until new year : {$minutesUntilNewYear}" . PHP_EOL;
 echo "Seconds until new year : {$secondsUntilNewYear}" . PHP_EOL;
-echo "Days:Hours:Minutes:Seconds {$formattedDaysUntilNewYear}:{$formattedHoursUntilNewYear}:{$formattedMinutesUntilNewYear}:{$formattedSecondsUntilNewYear}";
+echo "Days:Hours:Minutes:Seconds {$daysUntilNewYear}:{$formattedHoursUntilNewYear}:{$formattedMinutesUntilNewYear}:{$formattedSecondsUntilNewYear}";
