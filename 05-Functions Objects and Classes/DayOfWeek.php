5fa1724d4a37a77b4675d3f02d621238c979eb54
Martin Markov <martin.markov1980@gmail.com>
2024-02-19 08:48:43 +0000

    Completed "Problem Problem 2. Day of Week" task.

---
+++ 05-Functions Objects and Classes/DayOfWeek.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+  Problem 2. Day of Week
+  Write a function to return the day number by day of week in text.
+
+  Example.
+  Input:
+    Monday
+    Sunday
+    Hi
+
+  Output: 25
+    1
+    7
+    Invalid day!
+
+  Comments: None
+*/
+
+function dayOfWeek(string $day) : string {
+  switch ($day) {
+    case "Monday":
+      return "1";
+    case "Tuesday":
+      return "2";
+    case "Wednesday":
+      return "3";
+    case "Thursday":
+      return "4";
+    case "Friday":
+      return "5";
+    case "Saturday":
+      return "6";
+    case "Sunday":
+      return "7";
+    default:
+      return "Invalid day!";  
+  }
+}
+
+echo dayOfWeek("Thursday");
