bf3862e9b222c35934cdf4661b9dc819011c1738
Martin Markov <martin.markov1980@gmail.com>
2024-05-12 23:51:46 +0100

    Completed "Sort Lines" task.

--- 08-HTTP Basics Exercise/Sort Lines/sort_lines.php
+++ 08-HTTP Basics Exercise/Sort Lines/sort_lines.php
@@ -1,15 +1,19 @@
 <?php
 
-$cities = explode("\n", $_GET["text"]);
-// Removing the front and back empty space from each city
-$cities = array_map(function($e){
-  return trim($e);
-}, $cities);
-
-sort($cities, SORT_NATURAL | SORT_FLAG_CASE);
-$citiesOutput = "";
-foreach ($cities as $city) {
-  $citiesOutput .= $city . "\n";
+if (isset($_GET["text"])) {
+  $cities = explode("\n", $_GET["text"]);
+  // Removing the front and back empty space from each city
+  $cities = array_map("trim", $cities);
+  // Removing empty string array values 
+  $cities = array_filter($cities, function ($city) {
+    return $city != "";
+  });
+  // Sorting alphabetical case insensitive 
+  sort($cities, SORT_NATURAL | SORT_FLAG_CASE);
+  $citiesOutput = "";
+  foreach ($cities as $city) {
+    $citiesOutput .= $city . "\n";
+  }
 }
 
 include "./sort_lines.view.php";
--- 08-HTTP Basics Exercise/Sort Lines/sort_lines.view.php
+++ 08-HTTP Basics Exercise/Sort Lines/sort_lines.view.php
@@ -1,8 +1,6 @@
 <form action="./sort_lines.php">
   <div>
-    <textarea name="text" cols="30" rows="10">
-      <?= $citiesOutput ?>
-    </textarea>
+    <textarea name="text" cols="30" rows="10"><?= $citiesOutput ?></textarea>
   </div>
   <input type="submit" value="Sort">
 </form>
